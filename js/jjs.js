128f8e9122c8f35e66d319e5e0d32679f6e6cec4
JOJO <48712755+mettew0585@users.noreply.github.com>
2022-04-02 01:07:37 +0900

    final

--- js/jjs.js
+++ js/jjs.js
@@ -1,1 +1,12 @@
+const bioBtn = document.querySelector(".bio-btn");
+const contactBtn = document.querySelector(".contact-btn");
+const photoCho = document.querySelector(".photo__cho");
+const bio = document.querySelector(".bio");
 
+function bioBtnClick(e) {
+  console.log("dfsf");
+  photoCho.scrollIntoView({ behavior: "smooth" });
+  e.preventDefault();
+}
+
+bioBtn.addEventListener("click", bioBtnClick);
